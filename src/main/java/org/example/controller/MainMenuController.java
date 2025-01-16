1ffbed07f5041a99e983426c66740f63f83df652
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-16 18:34:04 -0500

    added loop in MainMenuController

--- src/main/java/org/example/controller/MainMenuController.java
+++ src/main/java/org/example/controller/MainMenuController.java
@@ -17,7 +17,32 @@ public class MainMenuController {
         boolean keepRunning = true;
 
         while (keepRunning) {
-            consoleIO.displayMessage("Shopping Cart App \uD83D\uDECD\uFE0F");
+            consoleIO.displayMessage("Welcome To Shopping Cart App \uD83D\uDECD\uFE0F");
+            consoleIO.displayMessage("Main Menu:");
+            consoleIO.displayMessage("1. Display Cart");
+            consoleIO.displayMessage("2. Remove an Item");
+            consoleIO.displayMessage("3. Add an Item");
+            consoleIO.displayMessage("4. Checkout");
+            consoleIO.displayMessage("5. Exit");
+
+            int choice = consoleIO.getInt("Choose an option: ");
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    consoleIO.displayMessage("Exiting the app. Goodbye! ");
+                    keepRunning = false;
+                    break;
+                default:
+                    consoleIO.displayMessage("Invalid input. Please try again");
+            }
         }
     }
 
