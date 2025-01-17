1944e98a73c711ab69a48fdde0a851ae21f77569
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-17 00:19:46 -0500

    experimented with README

---
+++ src/test/java/org/example/services/CartServiceTest.java
@@ -0,0 +1,24 @@
+package test.java.org.example.services;
+import org.junit.jupiter.api.Test;
+class CartServiceTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void displayCart() {
+    }
+
+    @Test
+    void calculateTotal() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
