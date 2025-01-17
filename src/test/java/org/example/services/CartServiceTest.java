5ea28465d8333177b3b3bec7b66d6e97286ec054
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-01-17 16:05:00 -0500

    added Test for addItem()

--- src/test/java/org/example/services/CartServiceTest.java
+++ src/test/java/org/example/services/CartServiceTest.java
@@ -1,9 +1,37 @@
 package test.java.org.example.services;
+import main.java.org.example.model.Cart;
+import main.java.org.example.model.Item;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CartServiceTest {
+
+    Cart testCart;
+
+    @BeforeEach
+    void setUp() {
+        testCart = new Cart();
+    }
 
     @Test
     void addItem() {
+        // arrange
+        testCart.addItem("apple", 20, 100);
+
+        // act
+        Map<String, Item> testMap = testCart.getItems();
+
+        //assert
+        assertTrue(testMap.containsKey("apple"));
+        assertEquals(20, testMap.get("apple").getPrice(), "Price should be 20");
+        assertEquals(100, testMap.get("apple").getQuantity(), "Quantity should be 100");
     }
 
     @Test
